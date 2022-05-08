69104dbdaeae2bdf070e03b5c845755cbc503f0d
yongbinkwon <yongbinkwon97@gmail.com>
2022-05-08 21:48:50 +0200

    fix bug where in BlacjackApplication where in some cases doing quit inputs does not stop console application + quit inputs with whitespace and/or capital letters are valid

--- src/main/kotlin/no/finn/blackjack/BlackjackApplication.kt
+++ src/main/kotlin/no/finn/blackjack/BlackjackApplication.kt
@@ -5,17 +5,17 @@ fun main() {
     while(true) {
         println("Enter the filename of the text file you want to use as a deck (inside the resources folder) " +
                 "or enter nothing if you want to use a predefined deck of 52 unique cards")
-        val deckName = readInput() ?: break
+        val deckName = readInput() ?: return
         while (true) {
             try {
                 val blackjack = if (deckName.isEmpty()) Blackjack() else Blackjack(Deck(addTrailingSlash(deckName)))
                 val result = blackjack.playRoundOfBlackjack(true)
                 println("${result.outcome()}\n")
                 println("play another round? y/n")
-                var playAnother = readInput() ?: break
+                var playAnother = readInput() ?: return
                 while (playAnother !in YES_INPUTS && playAnother !in NO_INPUTS) {
                     println("$playAnother is not a valid input, please type in 'y'/'yes' or 'n'/'no'")
-                    playAnother = readInput() ?: break
+                    playAnother = readInput() ?: return
                 }
                 if (playAnother in NO_INPUTS) break
             } catch (e: IllegalArgumentException) {
@@ -34,5 +34,6 @@ private val SERIES_OF_WHITESPACE_REGEX = Regex("(\\s+)")
 private fun addTrailingSlash(filePath: String) = if (filePath.first() == '/') filePath else "/$filePath"
 
 private fun readInput() = readLine().let {
-    if (it in QUIT_INPUTS) null else it?.replace(SERIES_OF_WHITESPACE_REGEX, "")?.lowercase()
+    val standardizedInput = it?.replace(SERIES_OF_WHITESPACE_REGEX, "")?.lowercase()
+    if (standardizedInput in QUIT_INPUTS) null else standardizedInput
 }
