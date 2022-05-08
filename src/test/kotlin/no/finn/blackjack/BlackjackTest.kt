55db98ba98e4546bbd909bfb60f1daccc6516366
yongbinkwon <yongbinkwon97@gmail.com>
2022-05-08 21:39:08 +0200

    change Blackjack tests to not only check winners but if the moves that are performed are correct + deck.offset can get bigger than deck.size

--- src/test/kotlin/no/finn/blackjack/BlackjackTest.kt
+++ src/test/kotlin/no/finn/blackjack/BlackjackTest.kt
@@ -2,6 +2,7 @@ package no.finn.blackjack
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 //just testing winners and not scores, because don't want to expose scores just for test purposes
 internal class BlackjackTest {
@@ -12,82 +13,249 @@ internal class BlackjackTest {
         }
     }
 
+    private fun initializeRound(sam: Player, dealer: Player, deck: Deck) {
+        sam.hit(deck.draw())
+        dealer.hit(deck.draw())
+        sam.hit(deck.draw())
+        dealer.hit(deck.draw())
+    }
+
     @Test
-    fun `if sam gets a blackjack they win`() {
-        val actualDeck = Deck("/BlackjackTest/SamBlackjack.txt")
+    fun `if sam gets a blackjack they win instantly`() {
+        val deckFilepath = "/BlackjackTest/SamBlackjack.txt"
+
+        val actualDeck = Deck(deckFilepath)
         val blackJack = Blackjack(actualDeck)
         val results = blackJack.playRoundOfBlackjack()
 
-        val expectedDeck = Deck("/BlackjackTest/SamBlackjack.txt")
+        val expectedDeck = Deck(deckFilepath)
         val expectedSam = Player.SAM()
         val expectedDealer = Player.DEALER()
 
-        expectedSam.hitMultiple(2, expectedDeck)
+        initializeRound(expectedSam, expectedDealer, expectedDeck)
 
-        assertEquals(Player.SAM(), results.winner)
+        assertAll(
+            { assertEquals(expectedSam, results.winner) },
+            { assertEquals(expectedDealer, results.loser) },
+            { assertEquals(expectedDeck, actualDeck)}
+        )
     }
 
     @Test
-    fun `if dealer gets a blackjack they win`() {
-        val blackJack = Blackjack(Deck("/BlackjackTest/DealerBlackjack.txt"))
+    fun `if dealer gets a blackjack they win instantly`() {
+        val deckFilepath = "/BlackjackTest/DealerBlackjack.txt"
+
+        val actualDeck = Deck(deckFilepath)
+        val blackJack = Blackjack(actualDeck)
         val results = blackJack.playRoundOfBlackjack()
-        assertEquals(Player.DEALER(), results.winner)
+
+        val expectedDeck = Deck(deckFilepath)
+        val expectedSam = Player.SAM()
+        val expectedDealer = Player.DEALER()
+
+        initializeRound(expectedSam, expectedDealer, expectedDeck)
+
+        assertAll(
+            { assertEquals(expectedSam, results.loser) },
+            { assertEquals(expectedDealer, results.winner) },
+            { assertEquals(expectedDeck, actualDeck)}
+        )
     }
 
     @Test
-    fun `if both players have blackjack sam wins`() {
-        val blackJack = Blackjack(Deck("/BlackjackTest/BothBlackjack.txt"))
-        val result = blackJack.playRoundOfBlackjack()
-        assertEquals(Player.SAM(), result.winner)
-    }
+    fun `if both players have blackjack sam wins instantly`() {
+        val deckFilepath = "/BlackjackTest/BothBlackjack.txt"
 
-    @Test
-    fun `if dealer gets a blackjack and sam has greater or equals 17, dealer wins`() {
-        val blackJack = Blackjack(Deck("/BlackjackTest/DealerBlackjackSamStands.txt"))
+        val actualDeck = Deck(deckFilepath)
+        val blackJack = Blackjack(actualDeck)
         val results = blackJack.playRoundOfBlackjack()
-        assertEquals(Player.DEALER(), results.winner)
+
+        val expectedDeck = Deck(deckFilepath)
+        val expectedSam = Player.SAM()
+        val expectedDealer = Player.DEALER()
+
+        initializeRound(expectedSam, expectedDealer, expectedDeck)
+
+        assertAll(
+            { assertEquals(expectedSam, results.winner) },
+            { assertEquals(expectedDealer, results.loser) },
+            { assertEquals(expectedDeck, actualDeck)}
+        )
+    }
+
+    @Test
+    fun `if dealer gets a blackjack and sam has greater or equals 17, dealer wins instantly`() {
+        val deckFilepath = "/BlackjackTest/DealerBlackjackSamStands.txt"
+
+        val actualDeck = Deck(deckFilepath)
+        val blackJack = Blackjack(actualDeck)
+        val results = blackJack.playRoundOfBlackjack()
+
+        val expectedDeck = Deck(deckFilepath)
+        val expectedSam = Player.SAM()
+        val expectedDealer = Player.DEALER()
+
+        initializeRound(expectedSam, expectedDealer, expectedDeck)
+
+        assertAll(
+            { assertEquals(expectedSam, results.loser) },
+            { assertEquals(expectedDealer, results.winner) },
+            { assertEquals(expectedDeck, actualDeck)}
+        )
     }
 
     @Test
     fun `if sam busts dealer wins without any further moves`() {
-        val blackJack = Blackjack(Deck("/BlackjackTest/SamBusts.txt"))
-        val result = blackJack.playRoundOfBlackjack()
-        assertEquals(Player.DEALER(), result.winner)
+        val deckFilepath = "/BlackjackTest/SamBusts.txt"
+
+        val actualDeck = Deck(deckFilepath)
+        val blackJack = Blackjack(actualDeck)
+        val results = blackJack.playRoundOfBlackjack()
+
+        val expectedDeck = Deck(deckFilepath)
+        val expectedSam = Player.SAM()
+        val expectedDealer = Player.DEALER()
+
+        initializeRound(expectedSam, expectedDealer, expectedDeck)
+        expectedSam.hitMultiple(1, expectedDeck)
+
+        assertAll(
+            { assertEquals(expectedSam, results.loser) },
+            { assertEquals(expectedDealer, results.winner) },
+            { assertEquals(expectedDeck, actualDeck)}
+        )
     }
 
     @Test
     fun `if dealer busts sam wins`() {
-        val blackJack = Blackjack(Deck("/BlackjackTest/DealerBusts.txt"))
+        val deckFilepath = "/BlackjackTest/DealerBusts.txt"
+
+        val actualDeck = Deck(deckFilepath)
+        val blackJack = Blackjack(actualDeck)
         val results = blackJack.playRoundOfBlackjack()
-        assertEquals(Player.SAM(), results.winner)
+
+        val expectedDeck = Deck(deckFilepath)
+        val expectedSam = Player.SAM()
+        val expectedDealer = Player.DEALER()
+
+        initializeRound(expectedSam, expectedDealer, expectedDeck)
+        expectedSam.hitMultiple(1, expectedDeck)
+        expectedDealer.hitMultiple(3, expectedDeck)
+
+        assertAll(
+            { assertEquals(expectedSam, results.winner) },
+            { assertEquals(expectedDealer, results.loser) },
+            { assertEquals(expectedDeck, actualDeck)}
+        )
     }
 
     @Test
-    fun `if both players busts on starting hand dealer wins`() {
-        val blackJack = Blackjack(Deck("/BlackjackTest/BothBustStartingHand.txt"))
+    fun `if both players bust on starting hand dealer wins`() {
+        val deckFilepath = "/BlackjackTest/BothBustStartingHand.txt"
+
+        val actualDeck = Deck(deckFilepath)
+        val blackJack = Blackjack(actualDeck)
         val results = blackJack.playRoundOfBlackjack()
-        assertEquals(Player.DEALER(), results.winner)
+
+        val expectedDeck = Deck(deckFilepath)
+        val expectedSam = Player.SAM()
+        val expectedDealer = Player.DEALER()
+
+        initializeRound(expectedSam, expectedDealer, expectedDeck)
+
+        assertAll(
+            { assertEquals(expectedSam, results.loser) },
+            { assertEquals(expectedDealer, results.winner) },
+            { assertEquals(expectedDeck, actualDeck)}
+        )
     }
 
     @Test
-    fun `sam correctly stands at 17 and dealer stands when total is higher`() {
-        val blackJack = Blackjack(Deck("/BlackjackTest/SamStandAt17DealerDoesNot.txt"))
+    fun `sam correctly stands at 17 and dealer stands when their score is higher than sam's`() {
+        val deckFilepath = "/BlackjackTest/SamStandAt17DealerDoesNot.txt"
+
+        val actualDeck = Deck(deckFilepath)
+        val blackJack = Blackjack(actualDeck)
         val results = blackJack.playRoundOfBlackjack()
-        assertEquals(Player.DEALER(), results.winner)
+
+        val expectedDeck = Deck(deckFilepath)
+        val expectedSam = Player.SAM()
+        val expectedDealer = Player.DEALER()
+
+        initializeRound(expectedSam, expectedDealer, expectedDeck)
+        expectedSam.hitMultiple(1, expectedDeck)
+        expectedDealer.hitMultiple(2, expectedDeck)
+
+        assertAll(
+            { assertEquals(expectedSam, results.loser) },
+            { assertEquals(expectedDealer, results.winner) },
+            { assertEquals(expectedDeck, actualDeck)}
+        )
     }
 
     @Test
     fun `sam can stand just based on starting hand`() {
-        val blackJack = Blackjack(Deck("/BlackjackTest/SamStandStartingHand.txt"))
+        val deckFilepath = "/BlackjackTest/SamStandStartingHand.txt"
+
+        val actualDeck = Deck(deckFilepath)
+        val blackJack = Blackjack(actualDeck)
         val results = blackJack.playRoundOfBlackjack()
-        assertEquals(Player.SAM(), results.winner)
+
+        val expectedDeck = Deck(deckFilepath)
+        val expectedSam = Player.SAM()
+        val expectedDealer = Player.DEALER()
+
+        initializeRound(expectedSam, expectedDealer, expectedDeck)
+        expectedDealer.hitMultiple(1, expectedDeck)
+
+        assertAll(
+            { assertEquals(expectedSam, results.winner) },
+            { assertEquals(expectedDealer, results.loser) },
+            { assertEquals(expectedDeck, actualDeck)}
+        )
     }
 
     @Test
     fun `sam wins without further moves if they get 21`() {
-        val blackJack = Blackjack(Deck("/BlackjackTest/Both21.txt"))
+        val deckFilepath = "/BlackjackTest/Both21.txt"
+
+        val actualDeck = Deck(deckFilepath)
+        val blackJack = Blackjack(actualDeck)
         val results = blackJack.playRoundOfBlackjack()
-        assertEquals(Player.SAM(), results.winner)
+
+        val expectedDeck = Deck(deckFilepath)
+        val expectedSam = Player.SAM()
+        val expectedDealer = Player.DEALER()
+
+        initializeRound(expectedSam, expectedDealer, expectedDeck)
+        expectedSam.hitMultiple(2, expectedDeck)
+
+        assertAll(
+            { assertEquals(expectedSam, results.winner) },
+            { assertEquals(expectedDealer, results.loser) },
+            { assertEquals(expectedDeck, actualDeck)}
+        )
+    }
+
+    @Test
+    fun `sam stand on starting hand even if dealer has a higher score`() {
+        val deckFilepath = "/BlackjackTest/SamLoseStartingHandNoBustOrBlackjack.txt"
+
+        val actualDeck = Deck(deckFilepath)
+        val blackJack = Blackjack(actualDeck)
+        val results = blackJack.playRoundOfBlackjack()
+
+        val expectedDeck = Deck(deckFilepath)
+        val expectedSam = Player.SAM()
+        val expectedDealer = Player.DEALER()
+
+        initializeRound(expectedSam, expectedDealer, expectedDeck)
+
+        assertAll(
+            { assertEquals(expectedSam, results.loser) },
+            { assertEquals(expectedDealer, results.winner) },
+            { assertEquals(expectedDeck, actualDeck)}
+        )
     }
 
 }
